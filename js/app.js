ed40e2f363306b2e9f7872e5acca8412bc52b287
Simon Oliehoek <simonbeep@gmail.com>
2021-03-05 21:48:22 +0100

    Implemented Node.js, moved static website to public folder

---
+++ js/app.js
@@ -0,0 +1,17 @@
+class Question{ 
+    problemStart = "";
+    constructor(title, problem, awnser){
+        this.title = title.toString();
+        this.problem = this.problemStart + problem;
+        this.awnser = awnser;
+    }
+    display(){
+        title = document.getElementsById("questiontitle")[0].setAttribute("text",this.title);
+    }
+    check(){
+
+    }
+}
+function asssesmentStart(){
+    console.log("test");
+}
