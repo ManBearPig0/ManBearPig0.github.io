4fe9e4c8b854ac2fc7defc38d9f7cc81179c10fd
Gust Verhees <gustverhees@gmail.com>
2021-03-17 13:20:06 +0100

    added question subclasses

--- public/js/components/question.js
+++ public/js/components/question.js
@@ -5,12 +5,49 @@ class Question {
         this.problem = this.problemStart + problem;
         this.awnser = awnser;
     }
-    display() {
-        title = document.getElementsById("questiontitle")[0].setAttribute("text", this.title);
-    }
-    check() {
+}
+Question.prototype.display = function(){
+    title = document.getElementsById("questiontitle")[0].setAttribute("text", this.title);
+}
+Question.prototype.check = function(input){
+    if(input == this.awnser){
 
     }
+}
+class multipleChoice extends Question{
+    problemStart = "Pick the right awnser: ";
+    constructor(title, problem, awnser, options){
+        super(title, problem, awnser);
+        this.options = options
+    }
+}
+multipleChoice.prototype.display = function(){
+    this.super.display();
+
+}
+
+class fillInTheBlank extends Question{
+    problemStart = "Fill in the black: "
+}
+fillInTheBlank.prototype.display = function(){
+    this.super.display();
+
+}
+
+var questions = []
+var initialDisplay = function(){
+    var main = document.getElementsByTagName("main")[0];
+    var pageTitle = document.createElement("h1");
+    pageTitle.appendChild(document.createTextNode("Assesment"))
+    var questionTitle = document.createElement("h2");
+
+    main.appendChild(pageTitle);
+}
+var correct =  function(){ 
+
+}
+var incorrect = function(){
+
 }
 
 const questionClass = new Question();
