0c2430c56869785657ab8d0906bb4945eba33543
Gust Verhees <gustverhees@gmail.com>
2021-03-17 18:08:20 +0100

    assesment multiple choice gemaakt

--- public/js/components/question.js
+++ public/js/components/question.js
@@ -1,47 +1,92 @@
 class Question {
     problemStart = "";
-    constructor(title, problem, awnser) {
-        this.title = title.toString();
+    constructor(ititle, problem, awnser) {
+        this.title = ititle.toString();
         this.problem = this.problemStart + problem;
         this.awnser = awnser;
     }
 }
 Question.prototype.display = function(){
-    title = document.getElementsById("questiontitle")[0].setAttribute("text", this.title);
+    var questionTitle = document.getElementById("question__title");
+    questionTitle.removeChild(questionTitle.childNodes[0]);
+    questionTitle.appendChild(document.createTextNode(this.title));
+
+    var questionProblem = document.getElementById("question__problem");
+    questionProblem.removeChild(questionProblem.childNodes[0]);
+    questionProblem.appendChild(document.createTextNode(this.problem));
+
+    var main = document.getElementsByTagName("main")[0];
+    while(main.children.length > 3){
+        main.removeChild(main.childNodes[2])
+    }
 }
 Question.prototype.check = function(input){
     if(input == this.awnser){
-
+        correct();
+    }
+    else{
+        incorrect();
     }
 }
 class multipleChoice extends Question{
     problemStart = "Pick the right awnser: ";
     constructor(title, problem, awnser, options){
         super(title, problem, awnser);
-        this.options = options
+        this.options = options;
+    }
+    display(){
+        super.display();
+        var main = document.getElementsByTagName("main")[0];
+        for(var option of this.options){
+            let paragraph = document.createElement("p");
+            let input = document.createElement("input");
+            input.setAttribute("type", "radio");
+            input.setAttribute("name", "vraag");
+            input.setAttribute("value", option);
+            input.id = option;
+            input.appendChild(document.createTextNode(option));
+            paragraph.appendChild(input);
+            let label = document.createElement("label");
+            label.setAttribute("for", option);
+            label.appendChild(document.createTextNode(option));
+            paragraph.appendChild(label);
+            main.insertBefore(paragraph, doceument.getElementById("question__check"));
+        }
     }
 }
-multipleChoice.prototype.display = function(){
-    this.super.display();
 
-}
 
 class fillInTheBlank extends Question{
     problemStart = "Fill in the black: "
+    display(){
+        super.display();
+    }
 }
-fillInTheBlank.prototype.display = function(){
-    this.super.display();
 
-}
+var questions = [new multipleChoice("Creator", "Who created laravel:", "Taylor Otwell", ["Sergey Sosnovsky", "Tim Berners-Lee", "Taylor Otwell", "Bill Gates"])];
 
-var questions = []
 var initialDisplay = function(){
     var main = document.getElementsByTagName("main")[0];
-    var pageTitle = document.createElement("h1");
-    pageTitle.appendChild(document.createTextNode("Assesment"))
-    var questionTitle = document.createElement("h2");
+    //create question heading
+    var questionTitle = document.createElement("h1");
+    questionTitle.id =  "question__title";
+    questionTitle.appendChild(document.createTextNode(""));
 
-    main.appendChild(pageTitle);
+    //create question
+    var questionProblem = document.createElement("h2");
+    questionProblem.id =  "question__problem";
+    questionProblem.appendChild(document.createTextNode(""));
+
+    //create submit button
+    var submitButton = document.createElement("button")
+
+
+    //add all elements to document
+    main.appendChild(questionTitle);
+    main.appendChild(questionProblem);
+
+    //display the first question
+    questions[0].display();
 }
 var correct =  function(){ 
 
@@ -49,8 +94,3 @@ var correct =  function(){
 var incorrect = function(){
 
 }
-
-const questionClass = new Question();
-
-// Export Module to be able to require() this file.
-module.exports = questionClass;
