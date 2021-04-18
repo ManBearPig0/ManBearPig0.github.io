8a4f753bd4fd4247cbf64cde7b6bf47a698f67ff
Gust Verhees <gustverhees@gmail.com>
2021-04-18 14:41:35 +0200

    question send to client

--- routes/quizRoutes.js
+++ routes/quizRoutes.js
@@ -2,26 +2,25 @@ import express from 'express';
 const router = express.Router();
 
 
-
 router.post('/question', (req, res, next) => {
     // do something
 });
 
 router.get("/getTopics", (req, res, next) => {
     var Topiclist = ["test1", "test2"]
-    var text = ""
-    for(var i = 0; i< Topiclist.length; i++){
-        text += Topiclist[i] + ",";
-    }
+    var text = JSON.stringify(Topiclist);
     res.send(text);
 })
 
 router.get("/getQuizes", (req, res, next) => {
     var Questionlist = ["quiz1","quiz2"];
-    var text = "";
-    for(var i = 0; i< Questionlist.length; i++){
-        text += Questionlist[i] + ",";
-    }
+    var text = JSON.stringify(Questionlist);;
+    res.send(text);
+})
+
+router.get("/getQuestion", (req, res, next) => {
+    var question = {title :"Creator", problem: "Who created laravel:", awnser : "Taylor Otwell", options:["Sergey Sosnovsky", "Tim Berners-Lee", "Taylor Otwell", "Bill Gates"]};
+    var text = JSON.stringify(question);
     res.send(text);
 })
 
